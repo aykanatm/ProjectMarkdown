d4c751bd0b09e8d5771eaabf7a8f0c788b9b57c4
Murat Aykanat <aykanatm@gmail.com>
2016-06-06 13:00:59 +0300

    Save feature added for testing.

--- ProjectMarkdown/ViewModels/MainWindowViewModel.cs
+++ ProjectMarkdown/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
 using ProjectMarkdown.Annotations;
 
 namespace ProjectMarkdown.ViewModels
@@ -13,6 +16,8 @@ namespace ProjectMarkdown.ViewModels
     public class MainWindowViewModel : INotifyPropertyChanged
     {
         private string _currentDocumentPath;
+        private string _currentText;
+        private string _currentHtml;
 
         public string CurrentDocumentPath
         {
@@ -25,7 +30,31 @@ namespace ProjectMarkdown.ViewModels
             }
         }
 
+        public string CurrentText
+        {
+            get { return _currentText; }
+            set
+            {
+                if (value == _currentText) return;
+                _currentText = value;
+                OnPropertyChanged(nameof(CurrentText));
+            }
+        }
+
+        public string CurrentHtml
+        {
+            get { return _currentHtml; }
+            set
+            {
+                if (value == _currentHtml) return;
+                _currentHtml = value;
+                OnPropertyChanged(nameof(CurrentHtml));
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand SaveDocumentCommand { get; set; }
 
         public MainWindowViewModel()
         {
@@ -33,8 +62,32 @@ namespace ProjectMarkdown.ViewModels
             {
                 return;
             }
+            using (var sr = new StreamReader("Example.html"))
+            {
+                CurrentHtml = sr.ReadToEnd();
+            }
+            LoadCommands();
             CurrentDocumentPath = "Untitled.md";
         }
+
+        private void LoadCommands()
+        {
+            SaveDocumentCommand = new RelayCommand(SaveDocument, CanSaveDocument);
+        }
+
+        public void SaveDocument(object obj)
+        {
+            using (var sr = new StreamReader("Example.html"))
+            {
+                CurrentHtml = sr.ReadToEnd();
+            }
+        }
+
+        public bool CanSaveDocument(object obj)
+        {
+            return true;
+        }
+
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
