456295b4e1f7dcd2135805370cec59b4b5a2ee37
Murat Aykanat <aykanatm@gmail.com>
2016-06-06 10:37:35 +0300

    Basic MVVM components added.

---
+++ ProjectMarkdown/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ProjectMarkdown.Annotations;
+
+namespace ProjectMarkdown.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _currentDocumentPath;
+
+        public string CurrentDocumentPath
+        {
+            get { return _currentDocumentPath; }
+            set
+            {
+                if (value == _currentDocumentPath) return;
+                _currentDocumentPath = value;
+                OnPropertyChanged(nameof(CurrentDocumentPath));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                return;
+            }
+            CurrentDocumentPath = "Untitled.md";
+        }
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
