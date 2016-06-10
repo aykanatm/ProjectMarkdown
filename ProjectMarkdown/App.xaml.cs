e315c4bb47ab985820fbea819fce2a496289f9d8
Murat Aykanat <aykanatm@gmail.com>
2016-06-10 15:19:14 +0300

    Awesomium browser implemented.

--- ProjectMarkdown/App.xaml.cs
+++ ProjectMarkdown/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Awesomium.Core;
 
 namespace ProjectMarkdown
 {
@@ -13,5 +14,30 @@ namespace ProjectMarkdown
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Initialization must be performed here,
+            // before creating a WebControl.
+            if (!WebCore.IsInitialized)
+            {
+                WebCore.Initialize(new WebConfig()
+                {
+                    HomeURL = "http://www.google.com".ToUri(),
+                    LogPath = @".\starter.log",
+                    LogLevel = LogLevel.Verbose
+                });
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Make sure we shutdown the core last.
+            if (WebCore.IsInitialized)
+                WebCore.Shutdown();
+
+            base.OnExit(e);
+        }
     }
 }
