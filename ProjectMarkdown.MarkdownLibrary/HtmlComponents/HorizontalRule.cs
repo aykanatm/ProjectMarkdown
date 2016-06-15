03e1fce7ed6f2b5ccd929e42c65d2b70cc0c8ffb
Murat Aykanat <aykanatm@gmail.com>
2016-06-15 16:44:08 +0300

    Added <hr> tag.

---
+++ ProjectMarkdown.MarkdownLibrary/HtmlComponents/HorizontalRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMarkdown.MarkdownLibrary.HtmlComponents
+{
+    public class HorizontalRule : HtmlComponent
+    {
+        public HorizontalRule() : base(null, TagTypes.HorizontalRule)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "<hr>";
+        }
+    }
+}
