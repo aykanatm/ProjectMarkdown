bb6f198682c2965807beb372a40b3355b53b671e
Murat Aykanat <aykanatm@gmail.com>
2016-06-16 16:37:00 +0300

    RawHtml component added.

---
+++ ProjectMarkdown.MarkdownLibrary/HtmlComponents/RawHtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMarkdown.MarkdownLibrary.HtmlComponents
+{
+    public class RawHtml : HtmlComponent
+    {
+        private readonly string _rawHtml;
+
+        public RawHtml(string rawHtml) : base(rawHtml, TagTypes.RawHtml)
+        {
+            _rawHtml = rawHtml;
+        }
+
+        public override string ToString()
+        {
+            return _rawHtml;
+        }
+    }
+}
