4d30dac5fc7432b844d14e8da6725ca9808d3900
Murat Aykanat <aykanatm@gmail.com>
2016-10-01 19:33:04 +0300

    Implemented ItemTemplate for document tab control.

---
+++ ProjectMarkdown/Model/DocumentModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Awesomium.Core;
+using ProjectMarkdown.Annotations;
+
+namespace ProjectMarkdown.Model
+{
+    public class DocumentModel : INotifyPropertyChanged
+    {
+        private string _header;
+        private string _markdownText;
+        private Uri _source;
+
+
+        public string Header
+        {
+            get { return _header; }
+            set
+            {
+                if (value == _header) return;
+                _header = value;
+                OnPropertyChanged(nameof(Header));
+            }
+        }
+
+        public string MarkdownText
+        {
+            get { return _markdownText; }
+            set
+            {
+                if (value == _markdownText) return;
+                _markdownText = value;
+                OnPropertyChanged(nameof(MarkdownText));
+            }
+        }
+
+        public Uri Source
+        {
+            get { return _source; }
+            set
+            {
+                if (Equals(value, _source)) return;
+                _source = value;
+                OnPropertyChanged(nameof(Source));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DocumentModel(string documentName)
+        {
+            Header = documentName;
+            MarkdownText = "";
+            Source = "".ToUri();
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- ProjectMarkdown/ViewModels/MainWindowViewModel.cs
+++ ProjectMarkdown/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
@@ -13,51 +14,43 @@ using System.Windows.Input;
 using Awesomium.Core;
 using ProjectMarkdown.Annotations;
 using ProjectMarkdown.MarkdownLibrary;
+using ProjectMarkdown.Model;
 
 namespace ProjectMarkdown.ViewModels
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
-        private string _currentDocumentPath;
-        private string _currentText;
-        private Uri _currentSource;
+        private ObservableCollection<DocumentModel> _documents;
+        private DocumentModel _currentDocument;
 
-        public string CurrentDocumentPath
+        public ObservableCollection<DocumentModel> Documents
         {
-            get { return _currentDocumentPath; }
+            get { return _documents; }
             set
             {
-                if (value == _currentDocumentPath) return;
-                _currentDocumentPath = value;
-                OnPropertyChanged(nameof(CurrentDocumentPath));
+                if (Equals(value, _documents)) return;
+                _documents = value;
+                OnPropertyChanged(nameof(Documents));
             }
         }
 
-        public string CurrentText
+        public DocumentModel CurrentDocument
         {
-            get { return _currentText; }
+            get { return _currentDocument; }
             set
             {
-                if (value == _currentText) return;
-                _currentText = value;
-                OnPropertyChanged(nameof(CurrentText));
+                if (Equals(value, _currentDocument)) return;
+                _currentDocument = value;
+                OnPropertyChanged(nameof(CurrentDocument));
             }
         }
 
-        public Uri CurrentSource
-        {
-            get { return _currentSource; }
-            set
-            {
-                if (value == _currentSource) return;
-                _currentSource = value;
-                OnPropertyChanged(nameof(CurrentSource));
-            }
-        }
-        
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ICommand SaveDocumentCommand { get; set; }
+        public ICommand CreateNewDocumentCommand { get; set; }
+        // Events
+        public ICommand MainWindowClosingEventCommand { get; set; }
 
         public MainWindowViewModel()
         {
@@ -67,51 +60,93 @@ namespace ProjectMarkdown.ViewModels
             }
             
             LoadCommands();
-            CurrentDocumentPath = "Untitled.md";
-
+            Documents = new ObservableCollection<DocumentModel>();
             
+            //CurrentDocumentPath = "Untitled.md";
 
-            using (var sr = new StreamReader("MarkdownTest.txt"))
-            {
-                string line;
-                do
-                {
-                    line = sr.ReadLine();
-                    CurrentText += line + "\r\n";
-                } while (line != null);
-            }
+            //using (var sr = new StreamReader("MarkdownTest.txt"))
+            //{
+            //    string line;
+            //    do
+            //    {
+            //        line = sr.ReadLine();
+            //        CurrentText += line + "\r\n";
+            //    } while (line != null);
+            //}
 
-            var mp = new MarkdownParser();
-            var html = mp.Parse(CurrentText);
-            using (var sw = new StreamWriter("MarkdownResult.html"))
-            {
-                sw.Write(html);
-            }
+            //var mp = new MarkdownParser();
+            //var html = mp.Parse(CurrentText);
+            //using (var sw = new StreamWriter("MarkdownResult.html"))
+            //{
+            //    sw.Write(html);
+            //}
 
-            CurrentSource = (AppDomain.CurrentDomain.BaseDirectory + "MarkdownResult.html").ToUri();
+            //CurrentSource = (AppDomain.CurrentDomain.BaseDirectory + "MarkdownResult.html").ToUri();
+            //CurrentSource = (AppDomain.CurrentDomain.BaseDirectory + "Empty").ToUri();
+            //CurrentFileName = "Untitled.md";
         }
 
         private void LoadCommands()
         {
             SaveDocumentCommand = new RelayCommand(SaveDocument, CanSaveDocument);
+            CreateNewDocumentCommand = new RelayCommand(CreateNewDocument, CanCreateNewDocument);
+            // Events
+            MainWindowClosingEventCommand = new RelayCommand(MainWindowClosingEvent, CanMainWindowClosingEvent);
+        }
+
+        public void CreateNewDocument(object obj)
+        {
+            int documentCount = 1;
+            string documentFileName = "untitled" + documentCount + ".md";
+            bool fileNameFound = false;
+            while (!fileNameFound)
+            {
+                var checkForDuplicateFileName = Documents.FirstOrDefault(d => d.Header == documentFileName);
+                if (checkForDuplicateFileName == null)
+                {
+                    fileNameFound = true;
+                }
+                else
+                {
+                    documentCount += 1;
+                    documentFileName = "untitled" + documentCount + ".md";
+                }
+            }
+            var document = new DocumentModel(documentFileName);
+            Documents.Add(document);
+        }
+        public bool CanCreateNewDocument(object obj)
+        {
+            return true;
         }
 
         public void SaveDocument(object obj)
         {
-            var mp = new MarkdownParser();
-            var html = mp.Parse(CurrentText);
-            using (var sw = new StreamWriter("MarkdownResult.html"))
-            {
-                sw.Write(html);
-            }
-            // Since Source property does not update when the same uri is called, we have to load some fake uri before we call the actual uri as a workaround
-            // https://github.com/awesomium/awesomium-pub/issues/52
-            // Will fix this when 1.7.5 is released
-            CurrentSource = "SomeFakeUri".ToUri();
-            CurrentSource = (AppDomain.CurrentDomain.BaseDirectory + "MarkdownResult.html").ToUri();
+            //var mp = new MarkdownParser();
+            //var html = mp.Parse(CurrentText);
+            //using (var sw = new StreamWriter(_currentHtmlFilePath))
+            //{
+            //    sw.Write(html);
+            //}
+            //// Since Source property does not update when the same uri is called, we have to load some fake uri before we call the actual uri as a workaround
+            //// https://github.com/awesomium/awesomium-pub/issues/52
+            //// Will fix this when 1.7.5 is released
+            //CurrentSource = "SomeFakeUri".ToUri();
+            //CurrentSource = _currentHtmlFilePath.ToUri();
         }
 
         public bool CanSaveDocument(object obj)
+        {
+            return true;
+        }
+
+        // EVENTS
+        public void MainWindowClosingEvent(object obj)
+        {
+            
+        }
+
+        public bool CanMainWindowClosingEvent(object obj)
         {
             return true;
         }
