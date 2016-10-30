68eeba7d9c4b2c203d1d1a44b02ef4c1b32fa016
Murat Aykanat <aykanatm@gmail.com>
2016-10-30 15:22:31 +0300

    Document loader updated.

--- ProjectMarkdown/Services/DocumentLoader.cs
+++ ProjectMarkdown/Services/DocumentLoader.cs
@@ -2,85 +2,98 @@
 using System.IO;
 using System.IO.Compression;
 using CustomIO;
+using Microsoft.Win32;
 using ProjectMarkdown.Model;
 
 namespace ProjectMarkdown.Services
 {
     public class DocumentLoader
     {
-        public DocumentModel Load(string filePath)
+        public DocumentModel Load()
         {
-            string currentMarkdown = "";
-            string currentHtml = "";
-            string currentXml = "";
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Markdown File | *.pmd";
+            var result = openFileDialog.ShowDialog();
 
-            using (var fs = new FileStream(filePath, FileMode.Open))
+            if (result != null)
             {
-                using (var archive = new ZipArchive(fs))
+                if (result == true)
                 {
-                    foreach (var entry in archive.Entries)
+                    string currentMarkdown = "";
+                    string currentHtml = "";
+                    string currentXml = "";
+
+                    using (var fs = new FileStream(openFileDialog.FileName, FileMode.Open))
                     {
-                        if (entry.Name.EndsWith(".md"))
+                        using (var archive = new ZipArchive(fs))
                         {
-                            using (var stream = entry.Open())
+                            foreach (var entry in archive.Entries)
                             {
-                                using (var zipSr = new StreamReader(stream))
+                                if (entry.Name.EndsWith(".md"))
                                 {
-                                    currentMarkdown = zipSr.ReadToEnd();
+                                    using (var stream = entry.Open())
+                                    {
+                                        using (var zipSr = new StreamReader(stream))
+                                        {
+                                            currentMarkdown = zipSr.ReadToEnd();
+                                        }
+                                    }
                                 }
-                            }
-                        }
-                        if (entry.Name.EndsWith(".html"))
-                        {
-                            using (var stream = entry.Open())
-                            {
-                                using (var zipSr = new StreamReader(stream))
+                                if (entry.Name.EndsWith(".html"))
                                 {
-                                    currentHtml = zipSr.ReadToEnd();
+                                    using (var stream = entry.Open())
+                                    {
+                                        using (var zipSr = new StreamReader(stream))
+                                        {
+                                            currentHtml = zipSr.ReadToEnd();
+                                        }
+                                    }
                                 }
-                            }
-                        }
-                        if (entry.Name.EndsWith(".xml"))
-                        {
-                            using (var stream = entry.Open())
-                            {
-                                using (var zipSr = new StreamReader(stream))
+                                if (entry.Name.EndsWith(".xml"))
                                 {
-                                    currentXml = zipSr.ReadToEnd();
+                                    using (var stream = entry.Open())
+                                    {
+                                        using (var zipSr = new StreamReader(stream))
+                                        {
+                                            currentXml = zipSr.ReadToEnd();
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+                    // Get metadata
+                    var gxs = new GenericXmlSerializer<DocumentMetadata>();
+                    var documentMetadata = gxs.DeSerializeFromString(currentXml);
+
+                    // Get markdown text
+                    var documentMarkdown = new DocumentMarkdown(currentMarkdown);
+
+                    // Get source URI
+                    var tempFolderPath = AppDomain.CurrentDomain.BaseDirectory + "Temp";
+                    if (!Directory.Exists(tempFolderPath))
+                    {
+                        Directory.CreateDirectory(tempFolderPath);
+                    }
+                    var tempSourceFilePath = tempFolderPath + "\\tempsource.html";
+                    using (var sw = new StreamWriter(tempSourceFilePath))
+                    {
+                        sw.Write(currentHtml);
+                    }
+                    var documentHtml = new DocumentHtml(new Uri(tempSourceFilePath));
+
+                    // Generate the model
+                    var documentModel = new DocumentModel(documentMetadata.FileName)
+                    {
+                        Metadata = documentMetadata,
+                        Html = documentHtml,
+                        Markdown = documentMarkdown
+                    };
+                    return documentModel;
                 }
+                return null;
             }
-            // Get metadata
-            var gxs = new GenericXmlSerializer<DocumentMetadata>();
-            var documentMetadata = gxs.DeSerializeFromString(currentXml);
-
-            // Get markdown text
-            var documentMarkdown = new DocumentMarkdown(currentMarkdown);
-
-            // Get source URI
-            var tempFolderPath = AppDomain.CurrentDomain.BaseDirectory + "Temp";
-            if (!Directory.Exists(tempFolderPath))
-            {
-                Directory.CreateDirectory(tempFolderPath);
-            }
-            var tempSourceFilePath = tempFolderPath + "\\tempsource.html";
-            using (var sw = new StreamWriter(tempSourceFilePath))
-            {
-                sw.Write(currentHtml);
-            }
-            var documentHtml = new DocumentHtml(new Uri(tempSourceFilePath));
-
-            // Generate the model
-            var documentModel = new DocumentModel(documentMetadata.FileName)
-            {
-                Metadata = documentMetadata,
-                Html = documentHtml,
-                Markdown = documentMarkdown
-            };
-            return documentModel;
+            return null;
         }
     }
 }
