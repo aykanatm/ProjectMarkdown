15682606d1232f422a37444c4e50e32352adb9fd
Murat Aykanat <aykanatm@gmail.com>
2016-11-05 20:48:05 +0300

    Pechkin html to pdf converter added.

---
+++ ProjectMarkdown.HtmlToPdfConverter/HtmlToPdfConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pechkin;
+
+namespace ProjectMarkdown.HtmlToPdfConverter
+{
+    public class HtmlToPdfConverter
+    {
+        public void Convert(string htmlString, string filePath)
+        {
+            byte[] pdfBuf = new SimplePechkin(new GlobalConfig()).Convert(htmlString);
+            File.WriteAllBytes(filePath, pdfBuf);
+        }
+    }
+}
