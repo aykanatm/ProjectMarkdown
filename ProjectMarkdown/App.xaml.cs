a62e53fd5237de5eab4add5d8c5671c26fd3e03d
Murat Aykanat <aykanatm@gmail.com>
2017-05-04 12:29:48 +0300

    Removed awesomium related code.

--- ProjectMarkdown/App.xaml.cs
+++ ProjectMarkdown/App.xaml.cs
@@ -14,32 +14,6 @@ namespace ProjectMarkdown
     /// </summary>
     public partial class App : Application
     {
-        /*
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            // Initialization must be performed here,
-            // before creating a WebControl.
-            if (!WebCore.IsInitialized)
-            {
-                WebCore.Initialize(new WebConfig()
-                {
-                    HomeURL = "http://www.google.com".ToUri(),
-                    LogPath = @".\starter.log",
-                    LogLevel = LogLevel.Verbose
-                });
-            }
-
-            base.OnStartup(e);
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            // Make sure we shutdown the core last.
-            if (WebCore.IsInitialized)
-                WebCore.Shutdown();
-
-            base.OnExit(e);
-        }
-        */
+        
     }
 }
