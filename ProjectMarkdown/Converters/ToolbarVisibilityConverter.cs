8b3c6e563b0d8fb70e13d47f20aa03efcf0de7e6
Murat Aykanat <aykanatm@gmail.com>
2017-08-19 15:36:16 +0300

    Implemented PM56 - Hide/Unhide toolbar.

---
+++ ProjectMarkdown/Converters/ToolbarVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectMarkdown.Converters
+{
+    [ValueConversion(typeof(bool), typeof(int))]
+    public class ToolbarVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var toolbarHeight = 30;
+
+            if (value != null)
+            {
+                var isToolbarHidden = (bool) value;
+                if (isToolbarHidden)
+                {
+                    toolbarHeight = 0;
+                }
+            }
+
+            return toolbarHeight;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
