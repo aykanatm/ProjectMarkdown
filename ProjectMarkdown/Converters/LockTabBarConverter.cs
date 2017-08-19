070c06c92a1cd552a0a1735e461652f206109fc8
Murat Aykanat <aykanatm@gmail.com>
2017-08-19 17:11:39 +0300

    Implemented PM58 - Ability to lock the the tabs so they cannot be dragged.

---
+++ ProjectMarkdown/Converters/LockTabBarConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectMarkdown.Converters
+{
+    [ValueConversion(typeof(bool), typeof(int))]
+    public class LockTabBarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fixedHeaderCount = 0;
+
+            if (value != null)
+            {
+                var isTabBarLocked = (bool)value;
+                if (isTabBarLocked)
+                {
+                    fixedHeaderCount = 50000;
+                }
+            }
+
+            return fixedHeaderCount;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
