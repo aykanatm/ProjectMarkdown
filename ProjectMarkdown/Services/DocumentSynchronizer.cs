09a2012fe1eb2ffe24469477c5fcdfe774a2a2dc
Murat Aykanat <aykanatm@gmail.com>
2017-09-03 20:03:32 +0300

    Fixed PM76 - When in sync mode, formatting changes produce empty strings on HTML view.

--- ProjectMarkdown/Services/DocumentSynchronizer.cs
+++ ProjectMarkdown/Services/DocumentSynchronizer.cs
@@ -7,11 +7,31 @@ namespace ProjectMarkdown.Services
 {
     public static class DocumentSynchronizer
     {
+        private static string _syncTemp1 = "SyncTemp_1.html";
+        private static string _syncTemp2 = "SyncTemp_2.html";
+        private static string _currentTempFileName;
+
         public static string Sync(DocumentModel document, string style)
         {
+            if (string.IsNullOrEmpty(_currentTempFileName))
+            {
+                _currentTempFileName = _syncTemp1;
+            }
+            else
+            {
+                if (_currentTempFileName == _syncTemp1)
+                {
+                    _currentTempFileName = _syncTemp2;
+                }
+                else
+                {
+                    _currentTempFileName = _syncTemp1;
+                }
+            }
+
             var mp = new MarkdownParser();
             var html = mp.Parse(document.Markdown, style);
-            var htmlFileName = "SyncTemp.html";
+            var htmlFileName = _currentTempFileName;
             // Generate HTML file
             var htmlFilePath = FolderPaths.TempFolderPath + "\\" + htmlFileName;
             using (var sw = new StreamWriter(htmlFilePath))
